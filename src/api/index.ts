0cb42c1615439ca16fb2a678c45054d8d7d62aa8
Louxinghong <1056868828@qq.com>
2023-06-13 09:31:13 +0800

    improvement：完善地图操作内容

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,14 @@
 import axios from '@/utils/axios'
+import AMapLoader from '@amap/amap-jsapi-loader'
 
 export const getViewData = () => axios.get('/x/web-interface/view')
+
+/**
+ * 获取高德引入结果
+ */
+export const getAMapData = () => {
+  return AMapLoader.load({
+    key: '7243d2aa043b480d7b4808c9fd90d78b', // 申请好的Web端开发者Key，首次调用 load 时必填
+    version: '1.4.15' // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+  })
+}
--- src/views/tool/coordinate-point/index.vue
+++ src/views/tool/coordinate-point/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="coordinate-point">
     <div class="actions">
-      <a-button class="btn-single" type="primary" size="mini" @click="onShowSingle"
-        >单点展示</a-button
-      >
-      <a-button class="btn-batch" type="primary" size="mini">批量展示</a-button>
+      <a-popover title="经纬度" trigger="click" position="bl">
+        <a-button class="btn-single" type="primary" size="small">单点展示</a-button>
+        <template #content>
+          <a-input class="input-lng" placeholder="请输入经度" allow-clear v-model="coordinateLng" />
+          <a-input class="input-lat" placeholder="请输入纬度" allow-clear v-model="coordinateLat" />
+          <a-button type="primary" @click="onDrawPoint">确定</a-button>
+        </template>
+      </a-popover>
+
+      <a-button class="btn-batch" type="primary" size="small">批量展示</a-button>
     </div>
 
     <div id="map-container" class="map-container"></div>
@@ -12,42 +18,35 @@
 </template>
 
 <script lang="ts" setup>
-import AMapLoader from '@amap/amap-jsapi-loader'
 import { ref, onMounted } from 'vue'
+import { getAMapData } from '@/api/index'
 
 let map = ref('')
-onMounted(() => {
-  console.log(`the component is now mounted.`)
-
-  AMapLoader.load({
-    key: '7243d2aa043b480d7b4808c9fd90d78b', // 申请好的Web端开发者Key，首次调用 load 时必填
-    version: '1.4.15' // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+let AMap = ref({})
+onMounted(async () => {
+  AMap = await getAMapData()
+  map = new AMap.Map('map-container', {
+    zoom: 11, //级别
+    center: [116.397428, 39.90923], //中心点坐标
+    viewMode: '2D' //使用3D视图
   })
-    .then((AMap) => {
-      map.value = new AMap.Map('map-container', {
-        zoom: 11, //级别
-        center: [116.397428, 39.90923], //中心点坐标
-        viewMode: '2D' //使用3D视图
-      })
-
-      const marker = new AMap.Marker({
-        icon: new AMap.Icon({
-          size: new AMap.Size(25, 34),
-          image: '//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png',
-          imageSize: new AMap.Size(135, 40),
-          imageOffset: new AMap.Pixel(-95, -3)
-        }),
-        position: [116.406315, 39.908775],
-        offset: new AMap.Pixel(-13, -30)
-      })
-      marker.setMap(map.value)
-    })
-    .catch((e) => {
-      console.log(e)
-    })
 })
 
-const onShowSingle = () => {}
+let coordinateLng = ref<string>('')
+let coordinateLat = ref<string>('')
+const onDrawPoint = () => {
+  const marker = new AMap.Marker({
+    icon: new AMap.Icon({
+      size: new AMap.Size(35, 35),
+      image: new URL('@/assets/images/point-marker.png', import.meta.url).href,
+      imageSize: new AMap.Size(35, 35),
+      imageOffset: new AMap.Pixel(0, 0)
+    }),
+    position: [coordinateLng.value, coordinateLat.value],
+    offset: new AMap.Pixel(-18, -18)
+  })
+  marker.setMap(map)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -62,6 +61,12 @@ const onShowSingle = () => {}
   .btn-batch {
     margin-left: 10px;
   }
+}
+
+.input-lng,
+.input-lat {
+  margin-right: 10px;
+  width: 120px;
 }
 
 .map-container {
