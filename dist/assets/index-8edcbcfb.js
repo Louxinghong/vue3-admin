8988cc08b690dce1b3a10d26530f81329f24d7b1
Louxinghong <1056868828@qq.com>
2023-11-13 14:20:20 +0800

    improvement：上传dist

---
+++ dist/assets/index-8edcbcfb.js
@@ -0,0 +1,1 @@
+import{_ as o,r as _,o as t,c as n,a as c}from"./index-61a85ff3.js";const s={},a={class:"foreign-page"};function r(d,i){const e=_("router-view");return t(),n("div",a,[c(e)])}const l=o(s,[["render",r],["__scopeId","data-v-7edd86a8"]]);export{l as default};
