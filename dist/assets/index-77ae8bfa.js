1ce05ce2514f5dff0dffca3278217b1955bb3a15
Louxinghong <1056868828@qq.com>
2024-02-27 17:30:38 +0800

    improvement：打包

--- dist/assets/index-77ae8bfa.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as _,o as t,c as n,a as c}from"./index-05298a00.js";const s={},a={class:"foreign-page"};function r(d,i){const e=_("router-view");return t(),n("div",a,[c(e)])}const l=o(s,[["render",r],["__scopeId","data-v-7edd86a8"]]);export{l as default};
